5dd24b53fbb782ea05ed802b4ccc4be10a352145
JateLi <597360550@qq.com>
2017-04-30 18:51:25 +1200

    testing purpose only

--- APPTest2/APPTest2/AppDelegate.swift
+++ APPTest2/APPTest2/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate , UITableViewDelegate{
+class AppDelegate: UIResponder, UIApplicationDelegate , UITableViewDelegate {
 
     var window: UIWindow?
 
