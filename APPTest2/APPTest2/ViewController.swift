ac1a1f6f6d98ebe4c61425b05fb2d96a519348c6
Tengzhe Li <lite0062@student.otago.ac.nz>
2017-10-05 16:51:26 +1300

    New BarChart

--- APPTest2/APPTest2/ViewController.swift
+++ APPTest2/APPTest2/ViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
-
+import Charts  // A third party library for charts generation.
 var Goal:Double = 0.0
 var Have:Double = 0.0
 
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var barChartView: BarChartView!
+    
    @IBOutlet weak var RandomNum: UITextField!
    @IBOutlet weak var RandomNum1: UILabel!
     @IBOutlet weak var saveButton: UIButton!
@@ -64,6 +65,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setBarChart(saved:5, range:10)
        let goalObject = UserDefaults.standard.object(forKey: "Goal")
         if (goalObject as? Double) != nil{
          Goal = goalObject as! Double
@@ -81,11 +83,179 @@ class ViewController: UIViewController {
     
     }
     
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-   
+    func setBarChart(saved: Double, range: Double){
+        
+        
+        
+        barChartView.drawBarShadowEnabled = false
+        
+        barChartView.drawValueAboveBarEnabled = false
+        
+        
+        
+        barChartView.maxVisibleCount = 100
+        
+        
+        
+        
+        
+        
+        
+        let xAxis  = barChartView.xAxis
+        
+        xAxis.labelPosition = .bottom
+        
+        xAxis.drawAxisLineEnabled = false
+        
+        xAxis.drawGridLinesEnabled = false
+        
+        xAxis.granularity = 0.0
+        
+        xAxis.enabled = false
+        
+        
+        
+        let leftAxis = barChartView.leftAxis;
+        
+        leftAxis.drawAxisLineEnabled = false;
+        
+        leftAxis.drawGridLinesEnabled = false;
+        
+        leftAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
+        
+        leftAxis.enabled = false;
+        
+        let rightAxis = barChartView.rightAxis
+        
+        rightAxis.enabled = false;
+        
+        
+        
+        rightAxis.drawAxisLineEnabled = false;
+        
+        rightAxis.drawGridLinesEnabled = false;
+        
+        rightAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
+        
+        rightAxis.enabled = false;
+        
+        let l = barChartView.legend
+        
+        l.enabled =  false
+        
+        
+        
+        barChartView.fitBars = true;
+        
+        barChartView.drawValueAboveBarEnabled = false;
+        
+        
+        
+        setDataCount(saved: saved, range:range)
+        
+        
+        
+    }
+    
+    
+    
+    func setDataCount( saved: Double, range: Double){
+        
+        
+        
+        let barWidth = 9.0
+        
+        let spaceForBar =  10.0;
+        
+        
+        
+        var yVals = [BarChartDataEntry]()
+        
+        
+        
+        
+        
+        
+        
+        yVals.append(BarChartDataEntry(x: Double(0) * spaceForBar, y: saved))
+        
+        
+        
+        yVals.append(BarChartDataEntry(x: Double(1) * spaceForBar, y: range))
+        
+        
+        
+        
+        
+        var set1 : BarChartDataSet!
+        
+        
+        
+        
+        
+        if let count = barChartView.data?.dataSetCount, count > 0{
+            
+            set1 = barChartView.data?.dataSets[0] as! BarChartDataSet
+            
+            set1.values = yVals
+            
+            
+            
+            barChartView.chartDescription?.text = ""
+            
+            barChartView.data?.notifyDataChanged()
+            
+            barChartView.notifyDataSetChanged()
+            
+            
+            
+            
+            
+        }else{
+            
+            set1 = BarChartDataSet(values: yVals, label: "")
+            
+            
+            
+           // colors of bars
+            set1.colors=[NSUIColor.blue,NSUIColor.white]
+            
+            var dataSets = [BarChartDataSet]()
+            
+            dataSets.append(set1)
+            
+            
+            
+            let data = BarChartData(dataSets: dataSets)
+            
+            data.setDrawValues(false)
+            
+            
+            
+            data.barWidth =  barWidth;
+            
+            
+            
+            barChartView.data = data
+            
+            barChartView.isUserInteractionEnabled = false;
+            
+            barChartView.animate(yAxisDuration: 1.5, easingOption: .easeInOutQuart)
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
 //    @IBAction func pushMe(_ sender: Any) {
 //        
 //        let randomNumber = getGaussian()
